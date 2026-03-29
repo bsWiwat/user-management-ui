9495c01a291459486cd1be582e1b01deae779632
bsWiwat <bs.wiwat@gmail.com>
2026-03-29 12:10:05 +0700

    init project usermanagement ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from "./components/topbar/topbar.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, SidebarComponent, TopbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'user-management-ui';
